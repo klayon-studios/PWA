a90202b85750edc2b0e9c58918f5fc7c57c21f92
freeed <liberatheon@gmail.com>
2023-11-01 04:39:55 +0700

    feat: add aa provider

--- src/components/Provider/index.tsx
+++ src/components/Provider/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { PrivyProvider } from "@privy-io/react-auth";
+import { SmartAccountProvider } from "@/hooks/SmartAccountContext";
 import { createPublicClient, http } from "viem";
 import { baseGoerli } from "viem/chains";
 import { env } from "../../../env.mjs";
@@ -10,14 +11,28 @@ export const publicClient = createPublicClient({
   transport: http(),
 });
 
-const handleLogin = (user: { id: string }) => {
-  console.log(`User ${user.id} logged in!`);
-};
+// const handleLogin = (user: { id: string }) => {
+//   console.log(`User ${user.id} logged in!`);
+// };
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <PrivyProvider appId={env.NEXT_PUBLIC_PRIVY_APP_ID} onSuccess={handleLogin}>
-      {children}
+    <PrivyProvider
+      appId={env.NEXT_PUBLIC_PRIVY_APP_ID}
+      config={{
+        loginMethods: ["email", "google"],
+        appearance: {
+          theme: "light",
+          accentColor: "#676FFF",
+        },
+        embeddedWallets: {
+          createOnLogin: "users-without-wallets",
+          noPromptOnSignature: true,
+        },
+        defaultChain: baseGoerli,
+      }}
+    >
+      <SmartAccountProvider>{children}</SmartAccountProvider>
     </PrivyProvider>
   );
 };
